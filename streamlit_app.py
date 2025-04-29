8440ed3860bc4a1cbfa214cbf6b806f198ba1a69
pjpjr <pjpjr@ix.netcom.com>
2025-04-29 09:25:09 -0400

    Update streamlit_app.py
    Testing new search_on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 # Customize with up to 5 fruits
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME',col('SEARCH_ON')
+                                                                          st.stop
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -34,7 +36,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
+        
 
 
     #st.write(ingredients_string)
