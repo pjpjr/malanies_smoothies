52683f015fc9d1a1b3bc699a5e903db98166185a
pjpjr <pjpjr@ix.netcom.com>
2025-04-29 10:23:52 -0400

    Update streamlit_app.py
    Testing adding search_on field

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,13 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie?')
 st.write('The name on your Smoothie will be: ', name_on_order)
+st.stop
 
 # Customize with up to 5 fruits
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     ,my_dataframe
