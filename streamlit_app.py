4567941c8fccde3fecbdd9be660f9152951c7c80
pjpjr <pjpjr@ix.netcom.com>
2025-04-28 22:48:12 -0400

    Update streamlit_app.py
    Moved section to display smoothifruit nutritional info

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,11 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
+    # New section to display smoothifruit nutritional information
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    #st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+
     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert = st.button('Submit Order!')
@@ -45,7 +50,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + ' !', icon="✅")
 
-# New section to display smoothifruit nutritional information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+
