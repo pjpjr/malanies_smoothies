1c47f70b7133121e2197e3dd7f0f601efcd21e4b
pjpjr <pjpjr@ix.netcom.com>
2025-04-29 09:28:44 -0400

    Update streamlit_app.py
    Fixing tabs/spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME',col('SEARCH_ON')
-                                                                          st.stop
+st.stop
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
